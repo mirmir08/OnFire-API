b6b89c410e9e45ed01e97b3b8aef423124e9383b
mirmir08 <mmrm.miriam@gmail.com>
2026-03-06 11:41:13 -0600

    Solución al error -11: respuesta rápida antes de Firebase

--- fire.js
+++ fire.js
@@ -1,14 +1,24 @@
-const admin = require('firebase-admin');
+// Usamos el modo compatibilidad para no tener que cambiar todo tu código
+const firebase = require('firebase/compat/app');
+require('firebase/compat/firestore');
 
-// Si configuraste la variable de entorno en Render como te sugerí:
-const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT 
-  ? JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT) 
-  : require("./tu-archivo-de-llaves.json"); // Por si pruebas localmente
+// Datos de tu proyecto OnFire!
+const config = {
+  apiKey: "apikey",
+  authDomain: "authDomain",
+  projectId: "projectId",
+  storageBucket: "storageBucket",
+  messagingSenderId: "messagingSenderId",
+  appId: "appId",
+  measurementId: "measurementId"
+};
 
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
-  });
+// Evitamos inicializar el app más de una vez
+let fire;
+if (!firebase.apps.length) {
+  fire = firebase.initializeApp(config);
+} else {
+  fire = firebase.app();
 }
 
-module.exports = admin;
+module.exports = fire;
